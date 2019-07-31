b00f99f779e342fd31642ab6ab0e144c086ec9a6
SiimonStark <justinpyktel@gmail.com>
2019-07-30 23:21:56 -0600

    Setup .env files

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,8 @@ import Nav from './components/Nav';
 import Welcome from './components/Welcome';
 import warnDaily from './components/warnDaily';
 import WordList from './components/WordList';
-import { key } from '../private';
+// import { key } from '../private';
+require('dotenv').config();
 
 export default {
   name: 'App',
@@ -45,14 +46,14 @@ export default {
       word !== '' ? this.fetchSynonyms(word) : null;
     },
     fetchSynonyms(word) {
-      axios.get(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}${key}`)
+      axios.get(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/?key=${word}${process.env.VUE_APP_SECRET}`)
       .then(res => this.foundWords = res.data)
       .then((res)=> console.log('Res: ',res))
       .catch(err => console.log(err));
     }
   },
   created() {
-    axios.get(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/day${key}`)
+    axios.get(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/day?key=${process.env.VUE_APP_SECRET}`)
       .then(res => this.defaultWords = res.data)
       .catch(err => console.log(err));
   },
